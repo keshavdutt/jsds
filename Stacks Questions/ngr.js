b18314d450fc19f10521d6000f9e7bae1dbbe5b8
keshavdutt <keshavdutt.1997@gmail.com>
2024-03-20 01:35:14 +0530

    nearest greatest to right

--- Stacks Questions/ngr.js
+++ Stacks Questions/ngr.js
@@ -1,54 +1,47 @@
-// Nearest greater to right
-// find the greatest element nearest to the right
+// // Nearest greater to right
+// // find the greatest element nearest to the right
 
-let some = [1, 3, 2, 4]
-
-// output => 3,4,4,-1
-let stack = []
-let temparray = [];
-
-// for (let i = some.length - 1; i >= 0; i--) {
-//     const element = some[i];
-//     if (stack.length == 0) {
-//         temparray.push(-1)
-//     }
-//     else if (stack.length > 0 && stack[stack.length - 1] > element) {
-//         temparray.push(stack[stack.length - 1])
-//     }
-//     else if (stack.length > 0 && stack[stack.length - 1] <= element) {
-//         while (stack.length > 0 && stack[stack.length - 1] <= element) {
-//             stack.pop()
-//         }
-//         if (stack.length == 0) {
-//             temparray.push(-1)
-//         } else {
-//             temparray.push(stack[stack.length - 1])
-//         }
-//     }
-//     stack.push(element)
-// }
-
-for (let i = some.length - 1; i >= 0; i--) {
-    const element = some[i];
-    while (stack.length > 0 && stack[stack.length - 1] <= element) {
-        stack.pop()
+function nearestGreaterToRight(input) {
+    let stack = [];
+    let result = [];
+    for (let i = input.length - 1; i >= 0; i--) {
+        const element = input[i];
+        while (stack.length > 0 && stack[stack.length - 1] <= element) {
+            stack.pop()
+        }
+        if (stack.length == 0) {
+            result.push(-1)
+        } else {
+            result.push(stack[stack.length - 1])
+        }
+        stack.push(element)
     }
-    if (stack.length == 0) {
-        temparray.push(-1)
-    } else {
-        temparray.push(stack[stack.length - 1])
-    }
-
-    stack.push(element)
-
+    return result.reverse();
 }
 
+// function nearestGreaterToRight(input) {
+//     let stack = [];
+//     let result = [];
 
-
-
-console.log(stack)
-console.log(temparray.reverse())
-
-
-
-function
+//     for (let i = input.length; i >= 0; i--) {
+//         const element = input[i];
+//         if (stack.length == 0) {
+//             result.push(-1)
+//         } else if (stack.length > 0 && stack[stack.length - 1] > element) {
+//             result.push(stack[stack.length - 1])
+//         } else if (stack.length > 0 && stack[stack.length - 1] <= element) {
+//             while (stack.length > 0 && stack[stack.length - 1] <= element) {
+//                 stack.pop()
+//             }
+//             if (stack.length == 0) {
+//                 result.push(-1)
+//             } else {
+//                 result.push(stack[stack.length - 1])
+//             }
+//         }
+//         stack.push(element);
+//     }
+//     return result.reverse();
+// }
+let input = [1, 3, 2, 4];
+console.log(nearestGreaterToRight(input))
