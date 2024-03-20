b6e4ca9b4c1d95f3408c52fdb19b55a2fd8a8ef7
keshavdutt <keshavdutt.1997@gmail.com>
2024-03-20 10:18:36 +0530

    Linkedlist question added

--- Stacks Questions/nsl.js
+++ Stacks Questions/nsl.js
@@ -1,48 +1,26 @@
 // Nearest smallest from left
 // find the smallest from the lest
 
-let array = [4,5,2,10,8]
+function nearestSmallestLeft(input) {
+    let stack = [];
+    let result = [];
+
+    for (let i = 0; i < input.length; i++) {
+        let element = input[i];
+        while (stack.length > 0 && stack[stack.length - 1] >= element) {
+            stack.pop()
+        }
+        if (stack.length === 0) {
+            result.push(-1)
+        } else {
+            result.push(stack[stack.length - 1])
+        }
+        stack.push(element)
+    }
+    return result
+}
+
+let input = [4, 5, 2, 10, 8];
 // output = -1, 4, -1, 2, 2
 
-class Stack {
-    constructor() {
-        this.items = [];
-    }
-    push (elements) {
-        this.items.push(elements);
-    }
-    pop (elements) {
-        if (this.items == 0) return "undefined"
-        return this.items.pop();
-    }
-    peek() {
-        return this.items[this.items.length - 1];
-    }
-    isEmpty() {
-        return this.items.length == 0
-    }
-}
-
-let stk = new Stack();
-let temparray = [];
-
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    if(stk.isEmpty()) {
-        temparray.push(-1)
-    } else if (!stk.isEmpty() && stk.peek() < element) {
-        temparray.push(stk.peek())
-    } else if (!stk.isEmpty() && stk.peek() >= element) {
-        while (!stk.isEmpty() && stk.peek() >= element) {
-            stk.pop()
-        }
-        if (stk.isEmpty()) {
-            temparray.push(-1)
-        } else {
-            temparray.push(stk.peek())
-        }
-    }
-    stk.push(element)
-}
-
-console.log(temparray)
+console.log(nearestSmallestLeft(input))
--- Stacks Questions/nsr.js
+++ Stacks Questions/nsr.js
@@ -1,50 +1,25 @@
 // Nearest smallest from right
 
-// let array = [4,5,2,10,8]
-let array = [ 4, 0, 0, 3, 0 ]
-// output 2,2,-1,8,-1
+function nearestSmallestRight(input) {
+    let stack = [];
+    let result = [];
 
-class Stack {
-    constructor() {
-        this.items = [];
-    }
-    push (elements) {
-        this.items.push(elements)
-    }
-    pop (elements) {
-        if (this.items == 0) return "undefined"
-        return this.items.pop();
-    }
-    peek() {
-        return this.items[this.items.length - 1];
-    }
-    isEmpty() {
-        return this.items.length == 0
-    }
-}
-
-let stk = new Stack();
-let temparray = []; 
-
-for (let i = array.length-1; i >= 0; i--) {
-    const element = array[i];
-    if (stk.isEmpty()) {
-        temparray.push(-1)
-    } else if (!stk.isEmpty() &&  stk.peek() < element) {
-        temparray.push(stk.peek())
-    } else if (!stk.isEmpty() && stk.peek() >= element) {
-        while (!stk.isEmpty() && stk.peek() >= element) {
-            stk.pop()
+    for (let i = input.length - 1; i >= 0; i--) {
+        let element = input[i];
+        while (stack.length > 0 && stack[stack.length - 1] >= element) {
+            stack.pop()
         }
-        if (stk.isEmpty()) {
-            temparray.push(-1)
+        if (stack.length === 0) {
+            result.push(-1)
         } else {
-            temparray.push(stk.peek())
+            result.push(stack[stack.length - 1])
         }
+        stack.push(element)
     }
-    stk.push(element)
-
-    
+    return result.reverse();
 }
 
-console.log(temparray.reverse())
+let input = [4, 5, 2, 10, 8];
+// output =  2,2,-1,8,-1
+
+console.log(nearestSmallestRight(input))
